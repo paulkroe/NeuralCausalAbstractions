b7a30cfe603357c26532001dc5739c3406aae401
Paul Kroeger <paul.kroeger@columbia.edu>
2025-03-20 10:19:50 -0400

    debugging gpu support on colab

--- src/pipeline/gan_pipeline.py
+++ src/pipeline/gan_pipeline.py
@@ -76,7 +76,7 @@ class GANPipeline(BasePipeline):
 
     def forward(self, n=1000, u=None, do={}, evaluating=False):
         out = self.ncm(n, u, do, evaluating=evaluating)
-        for v in out.values():
+        for k, v in out.items():
             out[k] = v.to(device=next(self.repr_model.decoders.parameters()).device)
         if self.repr_model is not None:
             out = self.repr_model.decode(out)
